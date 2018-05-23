7ff82bf4ccb1e75851c48205738b0d58383ab3fc
Sam Thomas <sgpthomas@gmail.com>
2018-05-23 13:49:25 -0700

    seperated figs into folders corresponding the method that they performed best on

--- compare.py
+++ compare.py
@@ -5,6 +5,8 @@ from sklearn.ensemble import GradientBoostingClassifier
 # from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 
+from progressbar import ProgressBar, Percentage, Bar, ETA
+
 import seaborn as sb
 import pandas as pd
 
@@ -26,21 +28,36 @@ def cca_fig(method, name, data):
     plt.savefig(join(path, name) + ".png", dpi=400)
     plt.clf()
 
+def make_progressbar(val):
+    return ProgressBar(widgets=[Percentage(),
+                                Bar(right="| "),
+                                ETA()],
+                       maxval=val)
+
+
 def compare_methods(names, m1, m2, m1_name="model1", m2_name="model2"):
     results = {m1_name: [], m2_name: []}
     row_names = {}
+    NUM = 6
+    pbar = make_progressbar(len(names) * NUM)
+    pbar.start()
     for i, dataset in enumerate(names):
         row_names[i] = dataset
 
         X, y = fetch_data(dataset, return_X_y=True)
-        train_X, test_X, train_y, test_y = train_test_split(X, y)
+        m1_score = 0
+        m2_score = 0
+        for j in range(NUM):
+            train_X, test_X, train_y, test_y = train_test_split(X, y)
 
-        m1.fit(train_X, train_y)
-        m2.fit(train_X, train_y)
+            m1.fit(train_X, train_y)
+            m2.fit(train_X, train_y)
 
-        m1_score = m1.score(test_X, test_y)
-        m2_score = m2.score(test_X, test_y)
-        print("m1: {}, m2: {}".format(m1_score, m2_score))
+            m1_score += m1.score(test_X, test_y)
+            m2_score += m2.score(test_X, test_y)
+            pbar.update(NUM*i + j)
+        m1_score /= NUM
+        m2_score /= NUM
         if m1_score > m2_score:
             results[m1_name].append(1)
             results[m2_name].append(0)
@@ -50,10 +67,11 @@ def compare_methods(names, m1, m2, m1_name="model1", m2_name="model2"):
             results[m2_name].append(1)
             cca_fig(m2_name, dataset, pd.DataFrame(X))
 
+    pbar.finish()
     df = pd.DataFrame(results)
     return df.rename(index=row_names)
 
-results = compare_methods(classification_dataset_names[:10],
+results = compare_methods(classification_dataset_names[127:],
                           LogisticRegression(),
                           GradientBoostingClassifier(),
                           m1_name="Logistic Regression",
